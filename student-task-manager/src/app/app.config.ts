8222dc18605192ed2461b3dda46684fb3525a8c1
Akshayraj33 <akshayrajchevala3@gmail.com>
2026-01-07 22:42:31 -0500

    app config typescript

--- student-task-manager/src/app/app.config.ts
+++ student-task-manager/src/app/app.config.ts
@@ -1,8 +1,23 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+// import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+// import { provideRouter } from '@angular/router';
 
-import { routes } from './app.routes';
+// import { routes } from './app.routes';
+
+// export const appConfig: ApplicationConfig = {
+//   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+// };
+
+import { ApplicationConfig } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { importProvidersFrom } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { TaskFormComponent } from './components/task-form/task-form.component';
+import { TaskListComponent } from './components/task-list/task-list.component';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideRouter([]),
+    importProvidersFrom(FormsModule)
+  ]
 };
