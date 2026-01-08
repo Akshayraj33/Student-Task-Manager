ceac0adb376ece88f93a31dca1fea9d120ad6b64
Akshayraj33 <akshayrajchevala3@gmail.com>
2026-01-07 22:43:09 -0500

    app module typescript

---
+++ student-task-manager/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { TaskFormComponent } from './components/task-form/task-form.component';
+import { TaskListComponent } from './components/task-list/task-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TaskFormComponent,
+    TaskListComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
